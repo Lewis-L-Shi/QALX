e4d175dcc83e6a0e9c1955feddda6f5d306fd961
Trang Quang <quang.tktrang@gmail.com>
2018-03-04 19:38:53 -0800

    Add authentication and authorization using Spring Security

---
+++ src/main/java/uwb/css553/qalx/config/MvcConfig.java
@@ -0,0 +1,17 @@
+package uwb.css553.qalx.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/doctor").setViewName("doctor");
+        registry.addViewController("/researcher").setViewName("researcher");
+    }
+}

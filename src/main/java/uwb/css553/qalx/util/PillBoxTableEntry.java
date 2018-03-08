7bf6c515582904ccfd460d733ed88262e758c693
Lewis <yishil@uw.edu>
2018-03-08 04:39:49 -0800

    Added SinglePatientRecord HTML page and attached PillBoxData to it.

---
+++ src/main/java/uwb/css553/qalx/util/PillBoxTableEntry.java
@@ -0,0 +1,56 @@
+package uwb.css553.qalx.util;
+
+public class PillBoxTableEntry {
+    private String psid;
+    private String date;
+    private String time;
+    private String statusStyle;
+    private String statusText;
+
+    public PillBoxTableEntry(String psid,String date, String time, String statusStyle, String statusText) {
+        this.psid = psid;
+        this.date = date;
+        this.time = time;
+        this.statusStyle = statusStyle;
+        this.statusText = statusText;
+    }
+
+    public String getPsid() {
+        return psid;
+    }
+
+    public String getDate() {
+        return date;
+    }
+    public String getStatusStyle() {
+        return statusStyle;
+    }
+
+    public String getStatusText() {
+        return statusText;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setPsid(String psid) {
+        this.psid = psid;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setStatusStyle(String statusStyle) {
+        this.statusStyle = statusStyle;
+    }
+
+    public void setStatusText(String statusText) {
+        this.statusText = statusText;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
